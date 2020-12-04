b7dff29c281409693901f2c2a5add1058a2bedb6
tmhuyett <toddhuyett@gmail.com>
2020-12-03 19:10:19 -0500

    'dfdfdfdfd'

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  
+    <><Head>
+ 
+    <script
+type="text/javascript"
+src="https://identity.netlify.com/v1/netlify-identity-widget.js">
+
+</script>
+  </Head>
+
+  <Component {...pageProps} /></>
 }
 
 export default Application
